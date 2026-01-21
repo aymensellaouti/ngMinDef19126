381a8f4368b26b329cdd929f18aa523fcab40957
aymensellaouti <aymen.sellaouti@gmail.com>
2026-01-21 11:35:05 +0100

    start exercice detailsCv

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { CvComponent } from './cv/cv-component/cv-component';
 import { Color } from './components/color/color';
 import { MiniWordComponent } from './directives/mini-word/mini-word.component';
 import { TodoComponent } from './todo/todo/todo.component';
-
+import { Second } from './components/second/second';
+// /cv
 export const routes: Routes = [
   {path: '', component: First},
   {path: 'cv', component: CvComponent},
   {path: 'color', component: Color},
   {path: 'word', component: MiniWordComponent},
   {path: 'todo', component: TodoComponent},
+  {path: ':unParam', component: Second},
 ];
--- src/app/components/second/second.ts
+++ src/app/components/second/second.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-second',
   imports: [],
-  template: `
-    <p>second works</p>
-  `,
+  template: ` <p>second works : {{ name() }}</p> `,
   styles: ``,
 })
 export class Second {
-
+  name = signal('');
+  acr = inject(ActivatedRoute);
+  constructor() {
+    this.name.set(this.acr.snapshot.params['unParam']);
+  }
 }
