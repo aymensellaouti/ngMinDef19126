38d84bfa1c741cdfa1a0c57dff4512cc0e8298fb
aymensellaouti <aymen.sellaouti@gmail.com>
2026-01-24 11:55:50 +0100

    fix bug

--- src/app/cv/cv.routes.ts
+++ src/app/cv/cv.routes.ts
@@ -6,7 +6,7 @@ import { authGuard } from '../auth/guards/auth-guard';
 import { cvResolver } from './resolvers/cv-resolver';
 
 export const CV_ROUTES: Routes = [
-  { path: '', loadComponent: () => import('./add-cv/add-cv.component').then(m => m.AddCvComponent) },
+  { path: '', loadComponent: () => import('./cv-component/cv-component').then(m => m.CvComponent) },
   { path: 'add', loadComponent: () => import('./add-cv/add-cv.component').then(m => m.AddCvComponent), canActivate: [authGuard] },
   {
     path: ':id',
